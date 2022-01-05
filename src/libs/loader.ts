7a7c0f5af71c807253264b6181819fc641c85de8
SoraKumo <info@croud.jp>
2022-01-05 13:56:07 +0900

    update package

--- src/libs/loader.ts
+++ src/libs/loader.ts
@@ -7,7 +7,6 @@ export const loader = ({
 }): Promise<unknown | undefined> =>
   fetch(`https://hacker-news.firebaseio.com/v0/${type}.json`)
     .then((v) => v.json())
-    //1秒のウエイト(これを入れないと早すぎて「読み込み中」の表示が見えない)
     .then(
       async (v) =>
         (await new Promise((r) =>
